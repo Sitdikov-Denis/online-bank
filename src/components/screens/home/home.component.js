fcfbeb2a1c99a35375651947be49c7134584f005
Denis Sitdikov <denis.sitdikov95@yandex.ru>
2024-02-10 23:35:41 +0300

    feat: render-service second step

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,13 +1,15 @@
-import { BaseScreen } from "@/components/base-screen.component";
-import RenderService from "@/core/services/render.service";
+import { BaseScreen } from '@/core/component/base-screen.component'
+import renderService from '@/core/services/render.service'
 
-export class Home extends BaseScreen{
-    constructor() {
-        super({title: 'Home'});
-        this.renderService = new RenderService()
-    }
-    render() {
-        const element = renderService.htmlElement(element)
-        return '<p>Home</p>'
-    }
+import template from './home.template.html'
+
+export class Home extends BaseScreen {
+	constructor() {
+		super({ title: 'Home' })
+	}
+
+	render() {
+		const element = renderService.htmlToElement(template)
+		return '<p>Home</p>'
+	}
 }
